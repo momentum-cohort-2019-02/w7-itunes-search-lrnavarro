62ac91377fce5706a9923bc016096fa9ccd6cd73
lrnavarro <linmery.navarro@gmail.com>
2019-03-30 19:36:05 -0400

    innerHtml to display single track

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    const submitButton = document.getElementById('submit');
+    const submitButton = document.getElementById('submit')
 
     submitButton.addEventListener('click', event => {
-        console.log("button clicked!");
+        console.log("button clicked!")
         searchTrack()
-    });    
-});
+    })   
+})
 
 function searchTrack() {
     //get user input
@@ -41,6 +41,17 @@ function getTracks(userInput) {
     //get tracks from iTunes API
     getTracks(userInput)
         .then(function (trackData) {
-            console.log(trackData)
+            // console.log(trackData)
+            const trackListDiv = document.getElementById('track-list')
+            const result = trackData.results[0]
+            const imageHtml = `<img src="${result.artworkUrl100}" />`
+            const trackNameDiv = `<div class="track-name">${result.trackCensoredName}</div>`
+            const trackArtistDiv = `<div class="track-artist">${result.artistName}</div>`
+            console.log(imageHtml)
+            console.log(trackNameDiv)
+            console.log(trackArtistDiv)
+            trackListDiv.innerHTML = `<div class="track">${imageHtml} ${trackNameDiv} ${trackArtistDiv}</div>`
+
         })
 }
+
